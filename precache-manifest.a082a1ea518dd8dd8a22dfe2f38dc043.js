e9915532e0e5cf5d448a28fdf5061b9b7583d9a0
marehbergo <mariaemilia.bergo@gmail.com>
2019-04-21 23:39:53 -0300

    Updates

---
+++ precache-manifest.a082a1ea518dd8dd8a22dfe2f38dc043.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe138339e21d91771b51",
+    "url": "/my-app/static/css/main.604565be.chunk.css"
+  },
+  {
+    "revision": "fe138339e21d91771b51",
+    "url": "/my-app/static/js/main.c3a2166c.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  },
+  {
+    "revision": "2e791f4a465440feb5b4",
+    "url": "/my-app/static/js/2.4524390a.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "add4c600c3800db4ca0fe20ecacedc06",
+    "url": "/my-app/index.html"
+  }
+];
